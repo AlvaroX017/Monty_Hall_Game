368f56682b3b74446645821d34a6d5ea6502b725
AlvaroX017 <alvarosousa722@gmail.com>
2022-10-19 23:31:59 -0300

    Monty Class Development

--- desktop/src/com/mygdx/game/Main.java
+++ desktop/src/com/mygdx/game/Main.java
@@ -2,7 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-//import com.mygdx.Monty;
+import com.mygdx.moneyhall.Monty;
 
 
 public class Main{
